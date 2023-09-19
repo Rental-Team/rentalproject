61b344712f6f49b2b57ec54da83905c58f2590f4
devpigKing <devpigking@gmail.com>
2023-09-20 00:36:45 +0900

    feat #2 : ItemController에 list를 띄워주는 list 메서드와 글을 등록하는 write 메서드, 상품 상세 보기의 get 메서드를 구현하였습니다.

--- workspace/rental-project/src/main/java/com/rentalproject/controller/ItemController.java
+++ workspace/rental-project/src/main/java/com/rentalproject/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.rentalproject.controller;
 
 
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,14 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rentalproject.dto.ItemDto;
 import com.rentalproject.service.ItemService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@Log4j
 @RequestMapping("/item")
 public class ItemController {
 	
 	@Autowired
 	private ItemService itemService;
-	
+
+	@GetMapping("/list")
+	public void list(Model model) {
+
+		log.info("list");
+
+		model.addAttribute("list", itemService.getList());
+	}
 	
 	@GetMapping("/write")
 	public String itemWriteForm() {
@@ -30,11 +41,23 @@ public class ItemController {
 	}
 	
 	@PostMapping("/write")
-	public String write(ItemDto item, Model model) {
-		
+	public String write(ItemDto item, RedirectAttributes rttr) {
+
+		log.info("register: " + item);
+
 		itemService.writeItem(item);
+		rttr.addAttribute("result", item.getItemNo());
+
+		return "redirect:/item/list";
+	}
+
+	@GetMapping("/get")
+	public void get(@RequestParam("itemNo") int itemNo, Model model) {
+
+		log.info("/get");
 		
-		return "redirect:home";
+		model.addAttribute("item", itemService.get(itemNo));
+
 	}
 
 }
