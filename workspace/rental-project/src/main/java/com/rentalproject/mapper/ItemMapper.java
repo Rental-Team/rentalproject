d358a7f50f1fc38a124a89e8e99b9328432eef72
devpigKing <devpigking@gmail.com>
2023-09-20 00:19:02 +0900

    feat #2 : ItemMapper.java에 리스트를 뽑아주는 getList()와 상품 상세 보기의 read() 메서드를 구현하였습니다.

--- workspace/rental-project/src/main/java/com/rentalproject/mapper/ItemMapper.java
+++ workspace/rental-project/src/main/java/com/rentalproject/mapper/ItemMapper.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 
 import com.rentalproject.dto.ItemDto;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ItemMapper {
@@ -12,7 +15,17 @@ public interface ItemMapper {
 	@Insert( "insert into item ( itemCode, itemName, itemDate, itemPrice "
 			+ "values ( #{ itemCode }, #{ itemName }, #{ itemDate }, #{ itemPrice } ")
 	@Options(useGeneratedKeys = true, keyProperty = "itemNo")
-	void insertItem(ItemDto item);
-	
-	
+	public void insertItem(ItemDto item);
+
+
+	@Select("select ( itemNo, itemCode, itemDate, itemPrice, itemDetail, itemPhoto) " +
+			"from item " +
+			"where itemNo > 0")
+	public List<ItemDto> getList();
+
+
+	@Select("select ( itemNo, itemCode, itemDate, itemPrice, itemDetail, itemPhoto) " +
+			"from item " +
+			"where itemNo = #{ itemNo } ")
+	public ItemDto read(int itemNo);
 }
