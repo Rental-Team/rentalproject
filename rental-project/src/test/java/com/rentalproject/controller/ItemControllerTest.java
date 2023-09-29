a0f63fce41429857788c28f826fca9097a015f60
devpigKing <devpigking@gmail.com>
2023-09-29 17:29:25 +0900

    이클립스 테스트 코드 예시

---
+++ rental-project/src/test/java/com/rentalproject/controller/ItemControllerTest.java
@@ -0,0 +1,24 @@
+package com.rentalproject.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ItemControllerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
