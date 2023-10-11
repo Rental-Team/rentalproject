9986a837dc5cc46231a4868c0e07526da9e0e016
devpigKing <devpigking@gmail.com>
2023-10-11 20:43:55 +0900

    feat : 주문 기능 구현 세팅

--- rental-project/src/main/java/com/rentalproject/controller/OrderController.java
+++ rental-project/src/main/java/com/rentalproject/controller/OrderController.java
@@ -1,18 +1,18 @@
 package com.rentalproject.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.rentalproject.dto.OrderPageDto;
 
 @Controller
+@RequestMapping("/rental")
 public class OrderController {
 
-	@GetMapping("/order/{memberNo}")
-	public void orderPage(@PathVariable("memberNo") int memberNo, OrderPageDto order, Model model) {
+	@GetMapping("/")
+	public String orderForm(){
 		
 		
+		return "rental/rentalRegister";
 	}
 }
